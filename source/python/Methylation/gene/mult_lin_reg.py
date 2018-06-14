86a6ac70a16912144bf09885fde22c5666232659
Unknown <hewitt.archie@yandex.ru>
2018-06-14 14:11:48 +0300

    OV comments

--- source/python/Methylation/gene/mult_lin_reg.py
+++ source/python/Methylation/gene/mult_lin_reg.py
@@ -9,6 +9,7 @@ from dicts import get_dicts
 from gen_files.geo import *
 import statsmodels.api as sm
 
+# Aux function for multiple regression
 def reg_m(y, x):
     ones = np.ones(len(x[0]))
     X = sm.add_constant(np.column_stack((x[0], ones)))
@@ -17,18 +18,21 @@ def reg_m(y, x):
     results = sm.OLS(y, X).fit()
     return results
 
+# Config
 fs_type = FSType.local_big
 db_type = DataBaseType.GSE40279
 geo_type = GeoType.islands_shores
 
-num_top = 100
-num_top_opt = 5
+num_top = 100 # Number of genes which compares with Claudio table
+num_top_opt = 5 # Number of genes in multiple regression
 
+# Claudio table
 fn = 'table.txt'
 full_path = get_full_path(fs_type, db_type, fn)
 file = open(full_path)
 table = file.read().splitlines()
 
+# ages
 fn = 'attribute.txt'
 ages = []
 full_path = get_full_path(fs_type, db_type, fn)
@@ -50,6 +54,7 @@ std_p_values = []
 std_slopes = []
 std_intercepts = []
 
+# Mean table
 fn = 'gene_mean' + geo_type.value + '.txt'
 full_path = get_full_path(fs_type, db_type, fn)
 f = open(full_path)
@@ -60,6 +65,7 @@ for line in f:
     mean_names.append(gene)
     mean.append(vals)
 
+# Std table
 fn = 'gene_std' + geo_type.value + '.txt'
 full_path = get_full_path(fs_type, db_type, fn)
 f = open(full_path)
@@ -70,6 +76,8 @@ for line in f:
     std_names.append(gene)
     std.append(vals)
 
+
+# Prioritizing by simple linear regression
 for id in range(0, len(mean_names)):
 
     means = mean[id]
@@ -95,8 +103,10 @@ order_mean = np.argsort(mean_p_values)
 genes_opt_mean = list(np.array(mean_names)[order_mean])
 mean_opt_mean = list(np.array(mean)[order_mean])
 
+# Multiple linear regression result
 print(reg_m(ages, mean_opt_mean[0:num_top_opt]).summary(xname=genes_opt_mean[0:num_top_opt], yname='age'))
 
+# Comparing with Claudio table
 genes_match = []
 mean_match = []
 for gene_id in range(0, num_top):
@@ -108,3 +118,21 @@ for gene_id in range(0, num_top):
 
 print('Claudio match')
 print(reg_m(ages, mean_match[0:num_top_opt]).summary(xname=genes_match[0:num_top_opt], yname='age'))
+
+# Dependence from variable count
+top_R = []
+top_coeff = []
+top_num = []
+
+for num_opt in range(0, num_top):
+    reg_res = reg_m(ages, mean_opt_mean[0:num_opt + 1])
+    top_num.append(num_opt + 1)
+    top_coeff.append(reg_res.rsquared)
+    top_R.append(reg_res.params[0])
+
+info = np.zeros(len(top_num), dtype=[('var1', int), ('var2', float), ('var3', float)])
+fmt = "%d %0.18e %0.18e"
+info['var1'] = top_num
+info['var2'] = top_coeff
+info['var3'] = top_R
+np.savetxt('R2.txt', info, fmt=fmt)
