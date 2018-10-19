76897bc026c37053db54a1dd49dcfcff8d844905
Unknown <hewitt.archie@yandex.ru>
2018-10-19 09:54:23 +0300

    plot changes

--- plot/local/get_metrics_label.m
+++ plot/local/get_metrics_label.m
@@ -1,10 +1,10 @@
-function metrics_label = get_metrics_label(config, rank)
+function metrics_label = get_metrics_label(config)
 metrics_label = '';
 
 if strcmp(config.method, 'linreg')
-    if rank == 1
+    if config.metrics_rank == 1
         metrics_label = 'r';
-    elseif rank == 2
+    elseif config.metrics_rank == 2
         metrics_label = 'slope';
     end
     
@@ -12,9 +12,9 @@ elseif strcmp(config.method, 'manova')
     metrics_label = '$-\log_{10}p\_value$';
     
 elseif strcmp(config.method, 'linreg_variance')
-    if rank == 1
+    if config.metrics_rank == 1
         metrics_label = 'r variance';
-    elseif rank == 2
+    elseif config.metrics_rank == 2
         metrics_label = 'r';
     end
     
--- plot/local/process_gender_specific_metrics.m
+++ plot/local/process_gender_specific_metrics.m
@@ -2,74 +2,66 @@ function [names, metrics_1, metrics_2] = process_gender_specific_metrics(config)
 names = [];
 metrics_1 = [];
 metrics_2 = [];
-if strcmp(config.method, 'manova')
-    if config.plot_method == 2
-        p_value_lim = 1e-8;
-        p_value_lim_log = -log10(p_value_lim);
-        metrics_1 = [];
-        metrics_2 = [];
-        for id = 1:size(target_metrics_1, 1)
-            if(target_metrics_1(id) < p_value_lim_log) || (target_metrics_2(id) < p_value_lim_log)
-                metrics_1 = vertcat(metrics_1, target_metrics_1(id));
-                metrics_2 = vertcat(metrics_2, target_metrics_2(id));
+
+config.gender = 'F';
+f_fn = sprintf('%s/data/%s/top.txt', ...
+    config.up, ...
+    get_result_path(config));
+f_top_data = importdata(f_fn);
+f_names = f_top_data.textdata;
+f_metrics = f_top_data.data(:, metrics_id);
+f_metrics = process_metrics(f_metrics, config);
+
+config.gender = 'M';
+m_fn = sprintf('%s/data/%s/top.txt', ...
+    config.up, ...
+    get_result_path(config));
+m_top_data = importdata(m_fn);
+m_names = m_top_data.textdata;
+m_metrics = m_top_data.data(:, metrics_id);
+m_metrics = process_metrics(m_metrics, config);
+
+all_names = f_names;
+num_names = size(all_names, 1);
+
+f_metrics_passed = f_metrics;
+m_metrics_passed = zeros(num_names, 1);
+m_order = zeros(num_names, 1);
+for id = 1:num_names
+    f_gene = f_names(id);
+    m_id = find(m_names==string(f_gene));
+    m_metrics_passed(id) = m_metrics(m_id);
+    m_order(id) = m_id;
+end
+
+if strcmp(config.data_type,'gene_data')
+    if strcmp(config.method, 'linreg')
+        if config.plot_method == 2
+            
+            p_value_lim = 1e-8;
+            p_value_lim_log = -log10(p_value_lim);
+            
+            config_1 = config;
+            config_1.metrics_rank = 2;
+            metrics_id_1 = 2;
+            f_metrics_1 = f_top_data.data(:, metrics_id_1);
+            m_metrics_1 = m_top_data.data(:, metrics_id_1);
+            m_metrics_1 = m_metrics_1(m_order);
+            f_metrics_1 = process_metrics(f_metrics_1, config_1);
+            m_metrics_1 = process_metrics(m_metrics_1, config_1);
+            
+            for id = 1:size(f_metrics_passed, 1)
+                if(f_metrics_1(id) > p_value_lim_log) || (m_metrics_1(id) > p_value_lim_log)
+                    names = vertcat(names, all_names(id));
+                    metrics_1 = vertcat(metrics_1, f_metrics_passed(id));
+                    metrics_2 = vertcat(metrics_2, m_metrics_passed(id));
+                end
             end
         end
     end
-    
-elseif strcmp(config.method, 'linreg')
-    
-    config.gender = 'F';
-    f_fn = sprintf('%s/data/%s/top.txt', ...
-        config.up, ...
-        get_result_path(config));
-    f_top_data = importdata(f_fn);
-    f_genes = f_top_data.textdata;
-    f_metrics = f_top_data.data(:, metrics_id);
-    f_metrics = process_metrics(f_metrics, config);
-    
-    config.gender = 'M';
-    m_fn = sprintf('%s/data/%s/top.txt', ...
-        config.up, ...
-        get_result_path(config));
-    m_top_data = importdata(m_fn);
-    m_genes = m_top_data.textdata;
-    m_metrics = m_top_data.data(:, metrics_id);
-    m_metrics = process_metrics(m_metrics, config);
-    
-    num_genes = size(f_genes, 1);
-    
-    f_metrics_passed = f_metrics;
-    m_metrics_passed = zeros(num_genes, 1);
-    for gene_id = 1:num_genes
-        f_gene = f_genes(gene_id);
-        m_id = find(m_genes==string(f_gene));
-        m_metrics_passed(gene_id) = m_metrics(m_id);
-    end
-    
-    [f_metrics_passed, m_metrics_passed] = process_metrics_plane(f_metrics_passed, m_metrics_passed, config);
-    num_bops = size(f_metrics_passed, 1);
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    if config.plot_method == 2
-        p_value_lim = 1e-8;
-        p_value_lim_log = -log10(p_value_lim);
-        metrics_1 = [];
-        metrics_2 = [];
-        for id = 1:size(target_metrics_1, 1)
-            if(target_metrics_1(id) < p_value_lim_log) || (target_metrics_2(id) < p_value_lim_log)
-                metrics_1 = vertcat(metrics_1, target_metrics_1(id));
-                metrics_2 = vertcat(metrics_2, target_metrics_2(id));
-            end
-        end
-    end
+else
+    names = vertcat(names, all_names(id));
+    metrics_1 = vertcat(metrics_1, target_metrics_1(id));
+    metrics_2 = vertcat(metrics_2, target_metrics_2(id));
 end
 end
