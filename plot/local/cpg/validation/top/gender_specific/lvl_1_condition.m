f5ff2186e444ad9dbad243480db417ea86aefcb5
Unknown <hewitt.archie@yandex.ru>
2018-11-16 18:18:51 +0300

    best list in progress

--- plot/local/cpg/validation/top/gender_specific/lvl_1_condition.m
+++ plot/local/cpg/validation/top/gender_specific/lvl_1_condition.m
@@ -1,19 +1,44 @@
-function passed_names = lvl_1_condition(config)
+function [passed_names, metrics_labels, metrics_data] = lvl_1_condition(config)
 [names, data_1, data_2] = get_gender_specific_data(config);
 
-sigma = 3;
-
-slopes_1 = data_1(:, 3);
-slopes_1_std = data_1(:, 5);
-
-slopes_2 = data_2(:, 3);
-slopes_2_std = data_2(:, 5);
-
-passed_names = [];
-for id = 1:size(names)
-    if slopes_1(id) < sigma * slopes_1_std(id) && slopes_2(id) < sigma * slopes_2_std(id)
-        passed_names = vertcat(passed_names, names(id));
+if strcmp(config.data_type, 'linreg_ols')
+    
+    if experiment == 1
+        
+        sigma = 3;
+        
+        slopes_1 = data_1(:, 3);
+        slopes_1_std = data_1(:, 5);
+        slopes_2 = data_2(:, 3);
+        slopes_2_std = data_2(:, 5);
+        
+        passed_names = [];
+        for id = 1:size(names)
+            if slopes_1(id) < sigma * slopes_1_std(id) && slopes_2(id) < sigma * slopes_2_std(id)
+                passed_names = vertcat(passed_names, names(id));
+            end
+        end
+        
+    elseif config.experiment == 2
+        
+        slope_lim = 0.002;
+        
+        slopes_1 = data_1(:, 3);
+        slopes_2 = data_2(:, 3);
+        
+        metrics_labels = ["slope_f"; "slope_m"];
+        
+        passed_names = [];
+        metrics_data = []
+        for id = 1:size(names)
+            if abs(slopes_1(id)) > slope_lim || abs(slopes_2(id)) > slope_lim
+                passed_names = vertcat(passed_names, names(id));
+                metrics_data = vertcat(metrics_data, [slopes_1(id), slopes_2(id)])
+            end
+        end
+        
     end
+    
 end
 
 end
--- plot/local/cpg/validation/top/gender_specific/processing_lvl_3.m
+++ plot/local/cpg/validation/top/gender_specific/processing_lvl_3.m
@@ -1,9 +1,13 @@
 clear all;
 
+config.experiment = 2;
+
 % ======== config ========
 config_lvl_1.data_base = 'GSE87571';
 config_lvl_1.data_type = 'cpg_data';
 
+config_lvl_1.cross_reactive = 'cross_reactive_excluded';
+config_lvl_1.snp = 'snp_included';
 config_lvl_1.chromosome_type = 'non_gender';
 
 config_lvl_1.dna_region = 'genic';
@@ -19,18 +23,14 @@ config_lvl_1.gender = 'versus';
 
 config_lvl_1.is_clustering = 0;
 
-if strcmp(getenv('computername'), 'MSI')
-    config_lvl_1.up = 'D:/YandexDisk/Work/mlmg';
-elseif strcmp(getenv('computername'), 'DESKTOP-4BEQ7MS')
-    config_lvl_1.up = 'D:/Aaron/Bio/mlmg';
-else
-    config_lvl_1.up = 'E:/YandexDisk/Work/mlmg';
-end
+config_lvl_1.up = get_up_data_path(); 
 
 % ======== save_config ========
 config_lvl_2.data_base = config_lvl_1.data_base;
 config_lvl_2.data_type = config_lvl_1.data_type;
 
+config_lvl_2.cross_reactive = config_lvl_1.cross_reactive;
+config_lvl_2.snp = config_lvl_1.snp;
 config_lvl_2.chromosome_type = config_lvl_1.chromosome_type;
 
 config_lvl_2.dna_region = config_lvl_1.dna_region;
@@ -44,15 +44,47 @@ config_lvl_2.method = 'gender_specific';
 config_lvl_2.disease = config_lvl_1.disease;
 config_lvl_2.gender = 'versus';
 
-config_lvl_2.up = config_lvl_1.up;
 config_lvl_2.is_clustering = config_lvl_1.is_clustering;
 
-lvl_1_names = lvl_1_condition(config_lvl_1);
-lvl_2_names = lvl_2_condition(config_lvl_1, config_lvl_2);
+config_lvl_2.up = config_lvl_1.up;
+
+[lvl_1_names, lvl_1_metrics_labels, lvl_1_metrics_data] = lvl_1_condition(config_lvl_1);
+[lvl_2_names, lvl_2_metrics_labels, lvl_2_metrics_data] = lvl_2_condition(config_lvl_1, config_lvl_2);
 
 intersect_names = intersect(lvl_1_names, lvl_2_names);
 
-suffix = sprintf('by_condition');
+metrics_data = [];
+metrics_labels = [];
+
+for name_id = 1:size(intersect_names,1)
+    name = intersect_names(name_id);
+    
+    
+end
+
+
+
+
+
+
+order = config.order;
+names = config.names;
+metrics_diff = config.metrics_diff;
+metrics_diff_labels = config.metrics_diff_labels;
+
+d = vertcat("names", string(names(order)));
+for metrics_id = 1:size(metrics_diff, 2)
+    d = horzcat(d, vertcat(metrics_diff_labels(metrics_id), string(metrics_diff(order, metrics_id))));
+end
+
+xlswrite(fn, d);
+
+
+
+
+
+
+suffix = sprintf('lvl_3_experiment(%s)', config.experiment);
 path = sprintf('%s/data/%s', ...
     config_lvl_1.up, ...
     get_result_path(config_lvl_2));
