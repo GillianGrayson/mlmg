f8e77bc243a884fe6cd5fbf07f6d3859d73f2722
Unknown <aaron.blare@mail.ru>
2018-11-30 12:19:40 +0300

    plot linreg_variance_added

---
+++ plot/local/cpg/approach/top/linreg_variance_ols/plot_linreg_variance_ols_cpg.m
@@ -0,0 +1,94 @@
+function plot_linreg_variance_ols_cpg(config, cpg)
+
+fn = sprintf('%s/data/%s/top.txt', ...
+    config.up, ...
+    get_result_path(config));
+
+top_data = importdata(fn);
+
+cpgs = string(top_data.textdata);
+
+intercepts = top_data.data(:, 2);
+slopes = top_data.data(:, 3);
+
+intercepts_var = top_data.data(:, 9);
+slopes_var = top_data.data(:, 10);
+intercepts_std_var = top_data.data(:, 11);
+slopes_std_var = top_data.data(:, 12);
+
+indexes = get_attributes_indexes(config);
+ages = get_ages(config);
+
+ages_passed = zeros(size(indexes, 1), 1);
+for id = 1:size(indexes, 1)
+    index = indexes(id);
+    ages_passed(id) = ages(index);
+end
+
+fn = sprintf('%s/data/%s/average_beta.txt', config.up, config.data_base);
+fid = fopen(fn);
+data = textscan(fid, '%s %*[^\n]','HeaderLines',1);
+frewind(fid)
+all_cpgs = data{1};
+idx = find(string(all_cpgs)==string(cpg))
+target_row = textscan(fid,'%s',1,'delimiter','\n', 'headerlines', idx-1);
+tline = strsplit(fgetl(fid), '\t');
+curr_cpg = string(tline(1));
+cpg_data = str2double(tline(2:end))';
+fclose(fid);
+
+cpg_data_passed = size(indexes, 1);
+for id = 1:size(indexes, 1)
+    cpg_data_passed(id) = cpg_data(indexes(id));
+end
+
+cpg_id = find(cpgs==cpg);
+
+slope = slopes(cpg_id);
+intercept = intercepts(cpg_id);
+diffs_data = size(indexes, 1);
+for id = 1:size(indexes, 1)
+    curr_x = ages_passed(id);
+    curr_y = cpg_data_passed(id);
+    pred_y = slope * curr_x + intercept;
+    diffs_data(id) = abs(pred_y - curr_y);
+end
+
+sigma = 3;
+
+slope_var = slopes_var(cpg_id);
+intercept_var = intercepts_var(cpg_id);
+intercept_std_var = intercepts_std_var(cpg_id);
+slope_std_var = slopes_std_var(cpg_id);
+
+x_lin = [min(ages), max(ages)];
+y_lin = [slope_var * x_lin(1) + intercept_var, slope_var * x_lin(2) + intercept_var];
+
+slope_minus_var = slope_var - sigma * slope_std_var;
+intercept_minus_var = intercept_var - sigma * intercept_std_var;
+
+slope_plus_var = slope_var + sigma * slope_std_var;
+intercept_plus_var = intercept_var + sigma * intercept_std_var;
+
+intercept_up_var = intercept_var + ((slope_plus_var * x_lin(2) + intercept_plus_var) - (slope_var * x_lin(2) + intercept_var));
+slope_up_var = slope_var;
+
+intercept_down_var = intercept_var + ((slope_minus_var * x_lin(2) + intercept_minus_var) - (slope_var * x_lin(2) + intercept_var));
+slope_down_var = slope_var;
+
+y_up_var = [slope_up_var * x_lin(1) + intercept_up_var, slope_up_var * x_lin(2) + intercept_up_var];
+y_down_var = [slope_down_var * x_lin(1) + intercept_down_var, slope_down_var * x_lin(2) + intercept_down_var];
+
+plot_data.scatter_x = ages_passed;
+plot_data.scatter_y = diffs_data;
+plot_data.line_x = x_lin;
+plot_data.line_y = y_lin;
+plot_data.line_y_down = y_down_var;
+plot_data.line_y_up = y_up_var;
+plot_data.line_name = sprintf('%s: %s', cpg, config.gender);
+plot_data.color = config.color;
+
+plot_linreg_variance_ols(plot_data)
+
+end
+
---
+++ plot/local/cpg/approach/top/linreg_variance_ols/regular.m
@@ -0,0 +1,112 @@
+clear all;
+
+cpgs = string(importdata('cpgs.txt'));
+
+% ======== config ========
+config.data_base = 'GSE87571';
+config.data_type = 'cpg_data';
+
+config.cross_reactive = 'cross_reactive_excluded';
+config.snp = 'snp_excluded';
+config.chromosome_type = 'non_gender';
+
+config.dna_region = 'genic';
+
+config.info_type = 'result';
+
+config.scenario = 'approach';
+config.approach = 'top';
+config.method = 'linreg_variance_ols';
+
+config.disease = 'any';
+config.gender = 'versus';
+
+config.is_clustering = 0;
+
+config.color = '';
+
+config.up = get_up_data_path(); 
+
+[annotations_map, labels] = get_annotations(config);
+
+for cpg_id = 1:size(cpgs, 1)
+    
+    cpg = cpgs(cpg_id)
+    
+    curr_ann = annotations_map(cpg);
+    genes_str = curr_ann(find(labels=="UCSC_REFGENE_NAME"));
+    genes = strsplit(genes_str,';');
+    genes = unique(genes);
+    genes = strjoin(genes, ';');
+    
+    % ======== processing ========
+    f = figure;
+    subplot(2,1,1);
+    if strcmp(config.gender, 'versus')
+        config.gender = 'F';
+        config.color = 'r';
+        plot_linreg_ols_cpg(config, cpg)
+        config.gender = 'M';
+        config.color = 'b';
+        plot_linreg_ols_cpg(config, cpg)
+        config.gender = 'versus';
+    else
+        plot_linreg_ols_cpg(config, cpg)
+    end
+   
+    box on;
+    b = gca;
+    legend(b,'off');
+    yl = ylim;
+    if yl(1) < 0
+       ylim([0, yl(2)]); 
+    end
+    if yl(2) > 1
+       ylim([yl(1), 1]);
+    end
+    title(sprintf('%s(%s)', cpg, genes), 'FontSize', 16)
+    
+    subplot(2,1,2);
+    if strcmp(config.gender, 'versus')
+        config.gender = 'F';
+        config.color = 'r';
+        plot_linreg_variance_ols_cpg(config, cpg)
+        config.gender = 'M';
+        config.color = 'b';
+        plot_linreg_variance_ols_cpg(config, cpg)
+        config.gender = 'versus';
+    else
+        plot_linreg_variance_ols_cpg(config, cpg)
+    end
+    
+    box on;
+    b = gca;
+    legend(b,'off');
+    yl = ylim;
+    if yl(1) < 0
+       ylim([0, yl(2)]); 
+    end
+    if yl(2) > 1
+       ylim([yl(1), 1]);
+    end
+    title(sprintf('%s(%s)', cpg, genes), 'FontSize', 16)
+    
+    if strcmp(getenv('computername'), 'MSI')
+        up_save = 'C:/Users/user/Google Drive/mlmg/figures';
+    elseif strcmp(getenv('computername'), 'DESKTOP-4BEQ7MS')
+        up_save = 'D:/Aaron/Bio/mlmg/figures';
+    else
+        up_save = 'C:/Users/user/Google Drive/mlmg/figures';
+    end
+    
+    save_path = sprintf('%s/%s', ...
+        up_save, ...
+        get_result_path(config));
+    mkdir(save_path);
+    
+    suffix = sprintf('cpg(%s)', cpg);
+    
+    savefig(f, sprintf('%s/%d_linreg_variance_%s.fig', save_path, cpg_id, suffix))
+    saveas(f, sprintf('%s/%d_linreg_variance_%s.png', save_path, cpg_id, suffix))
+    
+end
